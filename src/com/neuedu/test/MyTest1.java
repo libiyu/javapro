91d0a0a0fe00683642542281325bcfe362ac3cd9
libiyu <1527784916@qq.com>
2020-02-06 19:13:17 +0800

    MyTest1---第一个测试

---
+++ src/com/neuedu/test/MyTest1.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+/**
+ * Created by 86188 on 2020/2/6.
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
