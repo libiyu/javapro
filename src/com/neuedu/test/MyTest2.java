4efd6a8927e5c831b95265a3cf160a90cd2c17f3
libiyu <1527784916@qq.com>
2020-02-06 19:34:44 +0800

    MyTest2---第二个测试

---
+++ src/com/neuedu/test/MyTest2.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+/**
+ * Created by 86188 on 2020/2/6.
+ */
+public class MyTest2 {
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
